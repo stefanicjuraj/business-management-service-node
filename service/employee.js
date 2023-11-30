ef562e705538970b3ae623b7cd18337367c9740d
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-30 19:31:15 +0100

    feat: add get employee request route handler

--- service/employee.js
+++ service/employee.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const employee = require('../business/employee');
+
+// Route handler for an HTTP GET request
+router.get('/', (req, res) => {
+    const company = req.query.company;
+    const emp_id = req.query.emp_id;
+
+    if (company && emp_id) {
+        let response = employee.get(company, emp_id);
+        res.json(response);
+    } else if (company) {
+        let response = employee.getAll(company);
+        res.json(response);
+    } else {
+        res.status(400).json({ "error": "The company name AND/OR emp_id is missing." });
+    }
+});

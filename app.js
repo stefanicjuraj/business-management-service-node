7c1dd7f411a91c7fb4777e6195ad5c7a7d048e71
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-30 19:17:43 +0100

    feat: add app use & route handling

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const baseURL = "/BusinessManagementServiceNode/CompanyServices";
 
 const departmentRouter = require('./service/department');
 const employeeRouter = require('./service/employee');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// The 'departmentRouter' router handles department-related endpoints
+app.use(`${baseURL}/department`, departmentRouter);
+app.use(`${baseURL}/departments`, departmentRouter);
+
+// The 'employeeRouter' router handles employee-related endpoints
+app.use(`${baseURL}/employee`, employeeRouter);
+app.use(`${baseURL}/employees`, employeeRouter);

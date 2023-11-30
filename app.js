a2751cd4df7acd3727296b99ec7dd377f5707d2a
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-30 19:17:07 +0100

    feat: add department & employee router

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const app = express();
 
 const port = 3000;
 const baseURL = "/BusinessManagementServiceNode/CompanyServices";
+
+const departmentRouter = require('./service/department');
+const employeeRouter = require('./service/employee');

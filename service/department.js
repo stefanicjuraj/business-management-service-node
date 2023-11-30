08d8976b203f22349ff26ac5c7d3dd1456e62600
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-30 19:31:06 +0100

    feat: add get department request route handler

--- service/department.js
+++ service/department.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const department = require('../business/department');
+
+// Route handler for an HTTP GET request
+router.get('/', (req, res) => {
+    const company = req.query.company;
+    const dept_id = req.query.dept_id;
+
+    let response;
+
+    if (dept_id) {
+        response = department.get(company, dept_id);
+    } else {
+        response = department.getAll(company);
+    }
+
+    res.json(response);
+});

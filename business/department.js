2394bea5ff94b9adaa3842fd799476b2e68aa6ee
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-30 19:22:15 +0100

    feat: add department constructor

--- business/department.js
+++ business/department.js
@@ -3,5 +3,11 @@ const company_name = "js3199";
 var dl = new DataLayer(company_name);
 
 class Department {
-
+    constructor(company, dept_id, dept_name, dept_no, location) {
+        this.company = company;
+        this.dept_id = dept_id;
+        this.dept_name = dept_name;
+        this.dept_no = dept_no;
+        this.location = location;
+    }
 }
